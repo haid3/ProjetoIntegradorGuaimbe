98f70f0f8ffa0122da960889d7ad7213001c9674
JvRodrigues15 <joaovrc79@gmail.com>
2020-10-22 22:56:46 -0300

    ajustes entrega

--- resources/views/admin/transparencia/index.blade.php
+++ resources/views/admin/transparencia/index.blade.php
@@ -12,12 +12,19 @@
                         <fa class="fa fa-upload"></fa>
                 </button>
             </div>
-            
+                    
+        <div class="row">   
+            <div class="col-md-12">
+                <br>
+                    <center><img style="width:35%;height: auto;" src="https://thumbs.dreamstime.com/z/infographic-dashboard-template-flat-design-graphs-chart-infographic-dashboard-template-flat-design-graphs-charts-129188591.jpg" alt=""></center>
+            </div>
+        </div>
         {!! Form::close() !!}
     </div>
 </div>
 <div class="row">
 {!! Form::open(['route' => 'admin.transparencia.index', 'class' => 'form-horizontal form-produtos', 'method' => 'get']) !!}
+
     @include('admin.transparencia._partial.table')
     {!! Form::close() !!}
 </div>
