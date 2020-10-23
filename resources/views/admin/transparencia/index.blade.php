097831de8fcabee5a7d9a6d323e6875b22bc6e99
JvRodrigues15 <joaovrc79@gmail.com>
2020-10-22 22:36:32 -0300

    ajustes entrega

--- resources/views/admin/transparencia/index.blade.php
+++ resources/views/admin/transparencia/index.blade.php
@@ -5,18 +5,14 @@
 <div class="row">   
     <div class="col-md-12">
         {!! Form::open(['class' => 'import', 'route' => 'admin.transparencia.importCsv', 'method' => 'post','enctype' => "multipart/form-data", ]) !!}
-            <div class="text-right">
+            <div class="text-center">
                 {!! Form::file("excel", ['class' => 'btn btn-primary']) !!}
                 </a>
-            </div>
-            <br>
-            <div class="col-md-12">
-                <div class="form-group" align="right">
                 <button class="btn btn-success"> Importar
                         <fa class="fa fa-upload"></fa>
                 </button>
-                </div>
-            </div> 
+            </div>
+            
         {!! Form::close() !!}
     </div>
 </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,17 +8,30 @@
     <title>Transparência</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="../JS/Script.js"></script>
+    <!-- Gráficos -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script> 
+    
+    <!-- /Gráficos -->
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light mb-2 d-flex justify-content-between" style="background-color: #e9ecef;">
     </nav>
 
-    <div class="container">
-        <div class="jumbotron">
-            <h1>Transparencia</h1>
+    <div class="container2" style = "margin-right:15;margin-left:15 ">
+        <div class="jumbotron" style = "background:white;">
+            <center><h1 style="font-size:58px;">Gastos Publicos</h1></center>
         </div>
 
         @yield('conteudo')
     </div>
 </body>
 </html>
+
+.container1{width:100%;padding-right:15px;padding-left:15px;margin-right:auto;margin-left:auto}@media (min-width:576px){.container{max-width:540px}}@media (min-width:768px){.container{max-width:720px}}@media (min-width:992px){.container{max-width:960px}}@media (min-width:1200px){.container{max-width:1140px}}
